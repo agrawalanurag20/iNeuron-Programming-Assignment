8de0cb2dcdbc8b9c8b00f5774555f0345336035b
agrawalanurag20 <115839044+agrawalanurag20@users.noreply.github.com>
2022-10-21 19:26:14 +0530

    Add files via upload

---
+++ Programming Assignment_1.py
@@ -0,0 +1,34 @@
+# Program 1
+# Printing 'Hello Python'
+print("Hello Python")
+
+# Program 2
+# Arithmetic operation Addition and Division
+a = 8
+b = 2
+c = a + b
+d = c / b
+print(c)
+print(d)
+
+# Program 3
+# To find the area of a triangle
+b = int(input("Enter base:"))
+h = int(input("Enter Height:"))
+a = (b * h) / 2
+print(a)
+
+# Program 4
+# Swapping values of two variables
+a = 3
+b = 2
+print(f"Value of a:{a} and value of b:{b}")
+c = a
+a = b
+b = c
+print(f"After swap Value of a:{a} and value of b:{b}")
+
+# Program 5
+# Generate Random Number
+import random
+print(random.randint(0, 999))
